c63d441b152a140b84236894a8532e904d1f2ede
Nguyen <hyquaq@gmail.com>
2021-10-17 16:37:01 +0700

    huy update header footer responsive

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,15 @@
-// function removeThis(obj) {
-//     console.log(obj.parentElement);
-//     console.log(typeof obj.parentElement.noteName);
-//     // document.removeChild(obj.parentElement);
-//     obj.parentElement.remove();
-// }
+
 
 document.querySelector(".hamburger").addEventListener('click', () => {
 
     document.querySelector("header .menu").classList.toggle('active')
 })
+
+
+document.querySelectorAll('.menu a').forEach(
+    (element) => {
+        element.addEventListener('click', (e) => {
+            document.querySelector("header .menu").classList.toggle('active');
+        })
+    }
+)
