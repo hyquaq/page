796dff04fb338b6810467a66694c632accf8791c
phamkhoa1303 <pham.duykhoa1303@gmail.com>
2021-10-18 13:39:37 +0700

    chinh sua header

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,42 @@
+document.querySelector(".hamburger").addEventListener("click", () => {
+  document.querySelector("header .menu").classList.toggle("active");
+  document.querySelector("header .close-menu").classList.toggle("active");
+  console.log(document.querySelector("header .close-menu"));
+  // console.log(window.innerWidth <= 920);
+  // if (window.innerWidth <= 920) {
+  //     document.querySelector('.map iframe').classList.toggle('resize');
+  // }
+  console.log(window.innerWidth <= 920);
+  if (window.innerWidth <= 920) {
+    document.querySelector(".map iframe").classList.toggle("resize");
+  }
+  // khoa
+  document.querySelector("#chaomung").classList.toggle("farindex");
+});
 
+document.querySelectorAll(".menu a").forEach((element) => {
+  element.addEventListener("click", (e) => {
+    document.querySelector("header .menu").classList.toggle("active");
+    document.querySelector("header .close-menu").classList.remove("active");
+  });
+});
 
-document.querySelector(".hamburger").addEventListener('click', () => {
+window.addEventListener("scroll", function (event) {
+  var scroll = this.scrollY;
+  if (scroll > 200) {
+    document.querySelector("header").classList.remove("transparent");
+    document.querySelector("header").classList.add("animationHeader");
+  } else {
+    document.querySelector("header").classList.add("transparent");
+    document.querySelector("header").classList.remove("animationHeader");
+  }
+});
 
-    document.querySelector("header .menu").classList.toggle('active');
-    document.querySelector("header .close-menu").classList.toggle('active');
-    console.log(document.querySelector("header .close-menu"))
-    // console.log(window.innerWidth <= 920);
-    // if (window.innerWidth <= 920) {
-    //     document.querySelector('.map iframe').classList.toggle('resize');
-    // }
-    console.log(window.innerWidth <= 920);
-    if (window.innerWidth <= 920) {
-        document.querySelector('.map iframe').classList.toggle('resize');
-    }
-    // khoa
-    document.querySelector("#chaomung").classList.toggle('farindex');
-})
-
-
-document.querySelectorAll('.menu a').forEach(
-    (element) => {
-        element.addEventListener('click', (e) => {
-            document.querySelector("header .menu").classList.toggle('active');
-            document.querySelector("header .close-menu").classList.remove('active');
-        })
-    }
-)
-
-window.addEventListener("scroll", e => {
-
-})
-
-window.addEventListener("click", e => {
-    console.log([...e.target.classList].includes('close-menu'))
-    if ([...e.target.classList].includes('close-menu')) {
-        document.querySelector("header .menu").classList.remove('active');
-        document.querySelector("header .close-menu").classList.remove('active');
-        document.querySelector("#chaomung").classList.toggle('farindex');
-    }
-})
+window.addEventListener("click", (e) => {
+  console.log([...e.target.classList].includes("close-menu"));
+  if ([...e.target.classList].includes("close-menu")) {
+    document.querySelector("header .menu").classList.remove("active");
+    document.querySelector("header .close-menu").classList.remove("active");
+    document.querySelector("#chaomung").classList.toggle("farindex");
+  }
+});
