0f05476fc38d11b6a99c86f25fcaa2db4ad73f36
Nguyen <hyquaq@gmail.com>
2021-10-19 08:25:14 +0700

    fix menu tren mobile va chinh ux header

--- scripts/index.js
+++ scripts/index.js
@@ -2,28 +2,58 @@ document.querySelector(".hamburger").addEventListener("click", () => {
   document.querySelector("header .menu").classList.toggle("active");
   document.querySelector("header .close-menu").classList.toggle("active");
   console.log(document.querySelector("header .close-menu"));
+
+  // chuyen doi hamburger to x
+  document.querySelector(".hamburger").classList.toggle('active');
+
+
   // console.log(window.innerWidth <= 920);
   // if (window.innerWidth <= 920) {
   //     document.querySelector('.map iframe').classList.toggle('resize');
   // }
-  console.log(window.innerWidth <= 920);
-  if (window.innerWidth <= 920) {
-    document.querySelector(".map iframe").classList.toggle("resize");
-  }
+
+  // resize map
+  // console.log(window.innerWidth <= 920);
+  // if (window.innerWidth <= 920) {
+  //   document.querySelector(".map iframe").classList.toggle("resize");
+  // }
+
   // khoa
   document.querySelector("#chaomung").classList.toggle("farindex");
 });
 
+// click logo 
+document.querySelector("header .logo").addEventListener("click", (e) => {
+  document.querySelectorAll(".menu a").forEach(e => e.classList.remove('active'));
+
+  // element add class active 
+  document.querySelectorAll(".menu a")[0].classList.add('active');
+})
+
 document.querySelectorAll(".menu a").forEach((element) => {
   element.addEventListener("click", (e) => {
+
+    // all element remove class active
+    document.querySelectorAll(".menu a").forEach(e => e.classList.remove('active'));
+
+    // element add class active 
+    element.classList.add('active');
+
+
     document.querySelector("header .menu").classList.toggle("active");
+
+    // close menu 
     document.querySelector("header .close-menu").classList.remove("active");
+
+    // chuyen doi hamburger to x
+    document.querySelector(".hamburger").classList.toggle('active');
+
   });
 });
 
 window.addEventListener("scroll", function (event) {
   var scroll = this.scrollY;
-  if (scroll > 200) {
+  if (scroll > 20) {
     document.querySelector("header").classList.remove("transparent");
   } else {
     document.querySelector("header").classList.add("transparent");
@@ -35,6 +65,10 @@ window.addEventListener("click", (e) => {
   if ([...e.target.classList].includes("close-menu")) {
     document.querySelector("header .menu").classList.remove("active");
     document.querySelector("header .close-menu").classList.remove("active");
+
+    // chuyen doi hamburger to x
+    document.querySelector(".hamburger").classList.toggle('active');
+
     document.querySelector("#chaomung").classList.toggle("farindex");
   }
 });
