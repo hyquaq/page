aed9e2440bef6cf5f478b11135c5c5976316f926
duongdang2003 <64265100+duongdang2003@users.noreply.github.com>
2021-10-20 20:13:58 +0700

    Update thongtin.js

--- scripts/thongtin.js
+++ scripts/thongtin.js
@@ -1,4 +1,3 @@
-var button = document.querySelector('div #more');
 let incomingPush = true;
 let recentPush = true;
 let featurePush = true;
@@ -6,19 +5,15 @@ function pushIncoming() {
     let incoming = document.querySelector('#hoat-dong .bottom .incoming');
     let content = document.querySelector('#hoat-dong .incoming .content');
     let button = document.querySelector('.incoming #more');
-    let buttonMore = document.querySelector('#hoat-dong .incoming .more');
     if(incomingPush === true){
     button.className = 'bx bx-up-arrow';
-    content.style.height = '200px';
+    content.style.height = '250px';
     incoming.style.height = '580px';
-    buttonMore.style.display = 'block';
-    buttonMore.style.bottom = '50px';
     incomingPush = false;
     } else if(incomingPush === false){
         incoming.style.height = '380px';
         content.style.height = '50px';
         button.className = 'bx bx-down-arrow';
-        buttonMore.style.display = 'none';
         incomingPush = true;
     }
 }
@@ -26,18 +21,15 @@ function pushRecent() {
     let recent = document.querySelector('#hoat-dong .bottom .recent');
     let button = document.querySelector('.recent #more');
     let content = document.querySelector('#hoat-dong .recent .content');
-    let buttonMore = document.querySelector('#hoat-dong .recent .more');
     if(recentPush === true){
     recent.style.height = '580px';
-    content.style.height = '200px';
-    buttonMore.style.display = 'block';
+    content.style.height = '250px';
     button.className = 'bx bx-up-arrow';
     recentPush = false;
     } else if(recentPush === false){
         recent.style.height = '380px';
         content.style.height = '50px';
         button.className = 'bx bx-down-arrow';
-        buttonMore.style.display = 'none';
         recentPush = true;
     }
 }
@@ -45,18 +37,15 @@ function pushFeature() {
     let feature = document.querySelector('#hoat-dong .bottom .feature');
     let content = document.querySelector('#hoat-dong .feature .content');
     let button = document.querySelector('.feature #more');
-    let buttonMore = document.querySelector('#hoat-dong .feature .more');
     if(featurePush === true){
     feature.style.height = '580px';
-    content.style.height = '200px';
-    buttonMore.style.display = 'block';
+    content.style.height = '250px';
     button.className = 'bx bx-up-arrow';
     featurePush = false;
     } else if(featurePush === false){
         feature.style.height = '380px';
         content.style.height = '50px';
         button.className = 'bx bx-down-arrow';
-        buttonMore.style.display = 'none';
         featurePush = true;
     }
 }
